ab097b1aea76de647757b23fd19cf96e6f8326d5
alroad <alroad1234@hotmail.fr>
2012-12-17 11:57:15 +0100

    test de EchiquierActif

---
+++ echec/src/test/java/projet_echec/echec/jeu/EchiquierActifTest.java
@@ -0,0 +1,52 @@
+package projet_echec.echec.jeu;
+
+import junit.framework.TestCase;
+import projet_echec.echec.jeu.*;
+
+public class EchiquierActifTest extends TestCase{
+	
+	/**
+	 * test si lors d'une situation d'echec, la fonction testEchec() renvoit true
+	 */
+	public void testEchec(){
+		
+		assertTrue( true );
+		
+	}
+	
+	/**
+	 * test si lors d'une situation d'echec et mat, la fonction echecEtMat() renvoit true
+	 */
+	public void testechecEtMat(){
+		
+		assertTrue( true );
+		
+	}
+	
+	/**
+	 * test si lors d'une situation de pat, la fonction pat() renvoit true
+	 */
+	public void testpat(){
+		
+		assertTrue( true );
+		
+	}
+	/**
+	 * test lorsque l'interface renvoi une case Selectionné que l'on recupère bien la valeur dans CaseSelectionné
+	 */
+	public void testselectionnerCase(){
+		
+		assertTrue( true );
+	}
+	
+	/**
+	 * test lorsque l'on sélectionne deux cases l'un aprés l'autre, qu el'on déplace la pièce de la première à la deuxième case
+	 */
+	public void testdeplacer(){
+		
+		assertTrue( true );
+		
+	}
+	
+	
+}
