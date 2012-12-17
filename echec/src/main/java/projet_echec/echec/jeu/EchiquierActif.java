ab3a1763cc4c311879e4f3548b879a6fdac0b66a
alroad <alroad1234@hotmail.fr>
2012-12-17 10:48:56 +0100

    ajout des methodes et attributs de EchiquierActif

--- echec/src/main/java/projet_echec/echec/jeu/EchiquierActif.java
+++ echec/src/main/java/projet_echec/echec/jeu/EchiquierActif.java
@@ -1,4 +1,7 @@
 package projet_echec.echec.jeu;
+
+import java.util.Timer;
+
 /**
  * 
  * @author Adrien
@@ -7,5 +10,89 @@ package projet_echec.echec.jeu;
  * Cette classe permet de définir ce qu’il se passe sur l’échiquier lors d’une partie en cours.
  */
 public class EchiquierActif extends Echiquier {
-
+	/**
+	 * Temps restant au joueur avant la fin de son tour 
+	 */
+	Timer tempsRestant;
+	/**
+	 * Temps total du jeux depuis le début de la partie
+	 */
+	Timer tempsTotal;
+	/**
+	 * Case séléctionné par le joueur ( via l'interface graphique)
+	 */
+	Case caseSelectionne;
+	/**
+	 * case ou se situe le roi blanc
+	 */
+	Case caseRoiBlanc;
+	/**
+	 * case ou se situe le roi noir
+	 */
+	Case caseRoiNoir;
+	
+	
+	/**
+	 * constructeur de la classe
+	 */
+	public EchiquierActif(){
+		super();
+		
+	}
+	
+	/**
+	 * constructeur de la classe
+	 */
+	public EchiquierActif(Variantes V){
+		super(V);
+	}
+	
+	
+	/**
+	 * Test si on est en position d'échec
+	 * @return true si l'un des joueurs est en echec
+	 */
+	public boolean testEchec(){
+		boolean a;
+		a=true;
+		return a;
+	}
+	
+	/**
+	 * Test si on est en position d'échec et mat
+	 * @return true si l'un des joueurs est en echec et mat
+	 */
+	public boolean echecEtMat(){
+		boolean a;
+		a=true;
+		return a;
+		
+	}
+	/**
+	 * Test si on est en position de pat
+	 * @return true si l'un des joueurs est en pat
+	 */
+	public boolean pat(){
+		boolean a;
+		a=true;
+		return a;
+	}
+	/**
+	 * recupère la case selectionné envoyé par l'interface graphique
+	 * @param caseSelectionne
+	 */
+	public void selectionnerCase(Case caseSelectionne){
+		
+		
+	}
+	/**
+	 * Permet le déplacement des pièces
+	 * @param caseDepart : le joueur a selectionné une pièce sur le plateau
+	 * @param caseArrivee: le joueur a selectionné une case d'arrivée sur le plateau
+	 */
+	public void deplacer(Case caseDepart, Case caseArrivee){
+		
+	}
+	
+	
 }
