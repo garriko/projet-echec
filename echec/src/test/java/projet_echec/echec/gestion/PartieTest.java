1e169884502b02e02cf1d391f6a65c10c35ddf37
Kevin <kevg93@hotmail.fr>
2012-12-17 14:23:28 +0100

    Création classe de test

---
+++ echec/src/test/java/projet_echec/echec/gestion/PartieTest.java
@@ -0,0 +1,10 @@
+package projet_echec.echec.gestion;
+
+import junit.framework.TestCase;
+
+public class PartieTest extends TestCase {
+	
+	public void testAjoutDeplacement(){
+		assertTrue(true);
+	}
+}
