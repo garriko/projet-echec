dfd55d15dd749d98b5a0cca39929f90b1cafcc7d
Kevin <kevg93@hotmail.fr>
2012-12-17 15:19:41 +0100

    Déplacement cavalier

--- echec/src/main/java/projet_echec/echec/jeu/piece/Cavalier.java
+++ echec/src/main/java/projet_echec/echec/jeu/piece/Cavalier.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 import projet_echec.echec.jeu.Case;
 import projet_echec.echec.jeu.Piece;
+import projet_echec.echec.jeu.Position;
 
 
 /**
@@ -24,10 +25,42 @@ public class Cavalier extends Piece{
 	}
 
 	@Override
-	public ArrayList<Case> getDeplacementPossible(Case caseActuelle) {
+	public ArrayList<Case> getDeplacementPossible(Case pos) {
 		ArrayList<Case> lp = new ArrayList<Case>();
+		//On teste chacune des cases possibles
+		Case c0 = new Case(new Position(pos.getPosition().getHauteur()+2,pos.getPosition().getLargeur()+1));
+		if(isDeplacementOk(c0))
+			lp.add(c0);
 		
-		return null;
+		Case c1 = new Case(new Position(pos.getPosition().getHauteur()+2,pos.getPosition().getLargeur()-1));
+		if(isDeplacementOk(c1))
+			lp.add(c1);
+		
+		Case c2 = new Case(new Position(pos.getPosition().getHauteur()+1,pos.getPosition().getLargeur()+2));
+		if(isDeplacementOk(c2))
+			lp.add(c2);
+		
+		Case c3 = new Case(new Position(pos.getPosition().getHauteur()+1,pos.getPosition().getLargeur()-2));
+		if(isDeplacementOk(c3))
+			lp.add(c3);
+		
+		Case c4 = new Case(new Position(pos.getPosition().getHauteur()-2,pos.getPosition().getLargeur()-1));
+		if(isDeplacementOk(c4))
+			lp.add(c4);
+		
+		Case c5 = new Case(new Position(pos.getPosition().getHauteur()-2,pos.getPosition().getLargeur()+1));
+		if(isDeplacementOk(c5))
+			lp.add(c5);
+		
+		Case c6 = new Case(new Position(pos.getPosition().getHauteur()-1,pos.getPosition().getLargeur()-2));
+		if(isDeplacementOk(c6))
+			lp.add(c6);
+		
+		Case c7 = new Case(new Position(pos.getPosition().getHauteur()-1,pos.getPosition().getLargeur()+2));
+		if(isDeplacementOk(c7))
+			lp.add(c7);
+		
+		return lp;
 	}
 
 }
