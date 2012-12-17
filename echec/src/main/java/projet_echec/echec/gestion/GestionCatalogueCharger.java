69a9dfa99b4814c378493d823088d5adb156cd2a
Kevin <kevg93@hotmail.fr>
2012-12-17 09:50:11 +0100

    Ajout des commentaires dans le package gestion

--- echec/src/main/java/projet_echec/echec/gestion/GestionCatalogueCharger.java
+++ echec/src/main/java/projet_echec/echec/gestion/GestionCatalogueCharger.java
@@ -1,5 +1,17 @@
 package projet_echec.echec.gestion;
 
+/**
+ * 
+ * GestionCatalogueCharger gère les sauvegardes des parties non terminées.
+ * <p>
+ * GestionCatalogueCharger hérite de {@link Gestion}.
+ * 
+ * 
+ * @author Kevin
+ *@see Gestion
+ *@version 0.0.1
+ */
+
 public class GestionCatalogueCharger extends Gestion {
 
 }
--- echec/src/main/java/projet_echec/echec/gestion/GestionJeu.java
+++ echec/src/main/java/projet_echec/echec/gestion/GestionJeu.java
@@ -1,5 +1,16 @@
 package projet_echec.echec.gestion;
 
+/**
+ * GestionJeu gère le nombre de parties en cours.
+ * <p>
+ * Permet d'instancier une partie si le nombre de parties déjà existantes le permet.
+ * Supprime les objets parties.
+ *
+ * @author Kevin
+ * @version 0.0.1
+ * @see Partie
+ */
+
 public class GestionJeu {
 
 }
